85b5ea837f7623eed6c1b11c6efd18950891e33b
Makarov Anton <7evendays7@gmail.com>
2022-01-07 02:15:17 +0300

    async

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,32 +2,33 @@ const router = require("express").Router();
 const user = require("../models/user.js");
 const sequelize = require("../db/db");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { registerValidation } = require("../validation");
-  sequelize.sync().then((result) => {
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  sequelize.sync();
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const emailExist = async () =>
-      await user.findOne({ where: { email: req.body.email } });
-    if (emailExist) return res.status(400).send("Email is already exists");
-
-    try {
-      user
-        .create({
-          login: req.body.login,
-          password: req.body.password,
-          email: req.body.email,
-          phone_number: req.body.phone_number,
-        })
-        .then((response) => {
-          res.send("Register");
-          console.log(res);
-        });
-    } catch {
-      res.status(400).send(err);
-    }
+  const registredUser = await user.findOne({
+    where: { email: req.body.email },
   });
+
+  if (registredUser.id) return res.status(400).send("Email is already exists");
+
+  try {
+    user
+      .create({
+        login: req.body.login,
+        password: req.body.password,
+        email: req.body.email,
+        phone_number: req.body.phone_number,
+      })
+      .then((response) => {
+        res.send("Register");
+        console.log(res);
+      });
+  } catch {
+    res.status(400).send(err);
+  }
 });
 
 router.post("/login", (req, res) => {
